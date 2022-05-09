58e5cebf18ac300ef6bd669c29abd0725dc66f73
CodexDev <stefanga2020@gmail.com>
2022-05-09 19:23:23 +0200

    Managed to make Ads Images look alright. Working on putting the span in the image.

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -3,21 +3,39 @@ import React from 'react'
 function Ads() {
   return (
     <div className="flex flex-col p-5 bg-indigo-700 space-y-5">
-      <div className="font-bold text-xl text-white">
+      <div className="flex font-bold pt-2 text-xl text-white justify-center text-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, totam?
       </div>
       {/* IMAGES */}
-      <div className="flex space-x-2 max-w-screen-sm max-h-screen overflow-scroll">
-        <img
-          src="https://images.ctfassets.net/rb55mbb1m3hk/7ozdx92cZWiAtxzhEd2rTx/4f64792ce4e9a709c2519dd6662ce8ee/Kijiji_Lifestyle_20_Meetup-to-exchange-goods_horizontal_closeup_fall__1_.jpeg"
-          alt="meeting"
-          className="rounded-lg shadow-md"
-        />
-        <img
-          src="https://images.ctfassets.net/rb55mbb1m3hk/7ozdx92cZWiAtxzhEd2rTx/4f64792ce4e9a709c2519dd6662ce8ee/Kijiji_Lifestyle_20_Meetup-to-exchange-goods_horizontal_closeup_fall__1_.jpeg"
-          alt="meeting"
-          className="rounded-lg shadow-md"
-        />
+      {/* TODO: Make this responsive */}
+      <div className="flex w-full h-full justify-between space-x-4">
+        <a href="#" className="w-1/2">
+          <div>
+            <img
+              src="https://images.ctfassets.net/rb55mbb1m3hk/7ozdx92cZWiAtxzhEd2rTx/4f64792ce4e9a709c2519dd6662ce8ee/Kijiji_Lifestyle_20_Meetup-to-exchange-goods_horizontal_closeup_fall__1_.jpeg"
+              className="rounded-lg shadow-lg object-cover aspect-square"
+            />
+            {/*TODO:  DO THIS DIV'S INSIDE THE IMAGE */}
+            <div className="absolute">
+              <span className="text-sm text-white bg-zinc-600 bg-opacity-50">
+                Ipsum lorem?
+              </span>
+            </div>
+          </div>
+        </a>
+        <a href="#" className="w-1/2">
+          <div>
+            <img
+              src="https://images.ctfassets.net/rb55mbb1m3hk/7oMqQW63on7QFtmwRucu8q/c8d4af42bb85a0fb6db684c16b71d3da/top_5_home_improvements.jpeg"
+              className="rounded-lg shadow-lg object-cover aspect-square"
+            />
+            <div>
+              <span className="text-sm text-white bg-zinc-600 bg-opacity-50">
+                Ipsum lorem?
+              </span>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   )
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -6,12 +6,45 @@ function Post() {
     <div className="flex flex-col mx-auto overflow-hidden">
       <TopBar />
       <div className="flex p-5 mt-10 overflow-hidden justify-center">
-        <div className="ring-1 ring-black p-5 rounded-md space-y-10">
-          <div className="text-5xl font-bold flex-wrap">
-            List an auction item.
+        <div className="ring-1 ring-black p-5 w-full rounded-md space-y-8">
+          <div className="text-5xl font-bold whitespace-pre-line mb-[4rem]">
+            List an{'\n'} auction item.
           </div>
-          <div>(Placeholder for drag images)</div>
-          <div className="flex p-2 ring-1 ring-black rounded-sm justify-between">
+          <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+            <div className="space-y-1 text-center">
+              <svg
+                className="mx-auto h-12 w-12 text-gray-400"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <div className="flex text-sm text-gray-600">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                >
+                  <span>Upload a file</span>
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                  />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
+              <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            </div>
+          </div>
+          <div className="flex p-2 ring-1 ring-gray-400 rounded-sm justify-between">
             <div className="flex space-x-3">
               <img
                 src="https://via.placeholder.com/50x50"
@@ -19,7 +52,11 @@ function Post() {
                 className="rounded-sm"
               />
               <div className="flex flex-col">
-                <div className="text-md">img/2131231/321314/imd</div>
+                <a href="#">
+                  <div className="text-md font-semibold">
+                    img/2131231/321314/imd
+                  </div>
+                </a>
                 <div className="text-sm">Uploaded at 10:30 PM</div>
               </div>
             </div>
