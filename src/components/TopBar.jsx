eac7d88a614547783ce4c871cc417e2a1ac5170e
CodexDev <stefanga2020@gmail.com>
2022-05-20 15:25:45 +0200

    New HamburgerMenu animation and modal.

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -107,26 +107,59 @@ function TopBar() {
       <Transition
         as={Fragment}
         show={showHambuger}
-        enter="transition ease-in-out duration-500"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-250"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
+        enter="transition ease-in-out duration-800"
+        enterFrom="transform opacity-0 translate-x-[999px]"
+        enterTo="transform opacity-100 translate-x-0"
+        leave="transition ease-in duration-800"
+        leaveFrom="transform opacity-100 translate-x-0"
+        leaveTo="transform opacity-0 translate-x-[999px]"
       >
-        <div className="flex flex-col bg-white justify-around items-end space-y-2 text-xl font-semibold pb-2">
-          <a href="#categories" className="hover:underline">
-            Categories
-          </a>
-          <a href="#deals" className="hover:underline">
-            Deals
-          </a>
-          <a href="/post" className="hover:underline">
-            List Item
-          </a>
-          <a href="#help" className="hover:underline">
-            Help
-          </a>
+        <div className="h-screen w-full absolute right-0 top-0 bottom-0 left-0 justify-start items-end z-30 flex flex-col bg-white space-y-2 text-xl font-semibold pr-4">
+          <button
+            type="button"
+            onClick={() => setShowHambuger(false)}
+            className="flex space-x-1 items-center p-2 rounded-md hover:bg-gray-200"
+          >
+            <p>Close</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <div className="flex flex-col w-1/2 px-2 pl-4">
+            <a
+              href="#categories"
+              className="hover:underline border-b border-slate-300 w-full text-right py-4"
+            >
+              Categories
+            </a>
+            <a
+              href="#deals"
+              className="hover:underline border-b border-slate-300 w-full text-right py-4"
+            >
+              Deals
+            </a>
+            <a
+              href="/post"
+              className="hover:underline border-b border-slate-300 w-full text-right py-4"
+            >
+              List Item
+            </a>
+            <a
+              href="#help"
+              className="hover:underline border-b border-slate-300 w-full text-right py-4"
+            >
+              Help
+            </a>
+          </div>
         </div>
       </Transition>
       <SearchBar items={allCars} />
