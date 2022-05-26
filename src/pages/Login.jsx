d29f6aaf92368d2462da8195bc8f1d753bd95cd1
CodexDev <stefanga2020@gmail.com>
2022-05-26 21:14:52 +0200

    Added forgotPassword button on Login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className="flex flex-col p-5 max-w-7xl bg-bgColor m-5 rounded-sm">
+    <div className="flex flex-col p-10 py-20 max-w-7xl bg-bgColor m-5 rounded-sm">
       <div className="text-3xl font-bold self-center mb-10">Login</div>
       <form className="flex flex-col space-y-14">
         <div className="space-y-4">
           <input
             type="email"
             placeholder="mail@mail.com"
-            className="w-full p-3 rounded-md shadow-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
           />
           <input
             type="password"
             placeholder="Password"
-            className="w-full p-3 rounded-md shadow-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
           />
         </div>
         <button
@@ -24,6 +24,11 @@ function Login() {
           Login
         </button>
       </form>
+      <div className="flex mt-2 items-center justify-center">
+        <a href="#forgot" className="hover:underline opacity-75">
+          Forgot your password?
+        </a>
+      </div>
     </div>
   )
 }
