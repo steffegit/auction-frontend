73859d341031428cbd54e57d4d7eeecdff291a2a
CodexDEV <stefanga2020@gmail.com>
2022-06-01 21:00:40 +0300

    Added UserContextProvider to `App.js` and added `BidForm.jsx`

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
+// PAGES
 import Home from './pages/Home'
 import Post from './pages/Post'
 import NotFoundPage from './pages/NotFoundPage'
 import Bid from './pages/Bid'
 import Login from './pages/Login'
 import Search from './pages/Search'
+import BidForm from './pages/BidForm'
 
+// COMPONENTS
 import TopBar from './components/TopBar'
 import Footer from './components/Footer'
 
+// STYLES
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 import './styles.css'
 
+// OTHER
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { SiteContextProvider } from './components/Context/Context'
+import { UserContextProvider } from './components/Context/UserContext'
 
 function App() {
   return (
     <SiteContextProvider>
-      <Router>
-        <TopBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/bids/:id" element={<Bid />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <UserContextProvider>
+        <Router>
+          <TopBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post" element={<Post />} />
+            {/* this is just for preview bid */}
+            <Route path="/bids/:id" element={<Bid />} />
+            {/* and this is for form bid */}
+            <Route path="/bid/:id" element={<BidForm />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </UserContextProvider>
     </SiteContextProvider>
   )
 }
---
+++ src/pages/BidForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function BidForm() {
+  return <div>BidForm</div>
+}
+
+export default BidForm
