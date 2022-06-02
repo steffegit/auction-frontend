39769fd706903715af56b4294dc0b7aa75fb3b21
CodexDEV <stefanga2020@gmail.com>
2022-06-02 20:44:59 +0300

    Initial MultiStep form setup.

--- src/components/Form/FormSteps.jsx
+++ src/components/Form/FormSteps.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const steps = ['Pick Up', 'Financing', 'Bid Amount', 'Deposit Fee', 'Success']
 
 function FormSteps() {
+  const [currentStep, setCurrentStep] = useState(0)
+
   return (
     <div className="mx-4 p-4 flex justify-between items-center">
       <div className="w-full flex justify-center items-center sm:space-x-10">
         {steps.map((item, idx) => (
           <a
-            href={`#step${idx}`}
+            type="button"
+            onClick={() => {
+              setCurrentStep(idx)
+            }}
             className={`relative flex flex-col items-center text-gray-500 transition-all`}
           >
-            <div className="rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3">
+            <div
+              className={`rounded-full transition duration-500 ease-in-out border-2 ${
+                currentStep == idx ? 'border-green-400' : 'border-gray-300'
+              } h-12 w-12 flex items-center justify-center py-3`}
+            >
               {idx}
             </div>
             <div className="text-center mt-4 w-20 text-xs font-medium uppercase">
--- src/pages/BidForm.jsx
+++ src/pages/BidForm.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import FormSteps from '../components/Form/FormSteps'
+
+import BidAmount from '../components/Form/FormPages/BidAmount'
+import Deposit from '../components/Form/FormPages/Deposit'
+import Financing from '../components/Form/FormPages/Financing'
+import PickUp from '../components/Form/FormPages/PickUp'
+import Success from '../components/Form/FormPages/Success'
+
+const steps = ['Pick Up', 'Financing', 'Bid Amount', 'Deposit Fee', 'Success']
 
 function BidForm() {
   let { id } = useParams()
+  const [page, setPage] = useState(0)
+
+  const PageDisplay = () => {
+    switch (page) {
+      case 0:
+        return <PickUp />
+      case 1:
+        return <Financing />
+      case 2:
+        return <BidAmount />
+      case 3:
+        return <Deposit />
+      case 4:
+        return <Success />
+      default:
+        return <PickUp />
+    }
+  }
   return (
     <div className="flex flex-col max-w-7xl mx-auto scroll-smooth pb-5">
-      <FormSteps />
+      <div>Current: {steps[page]}</div>
+      <div>{PageDisplay()}</div>
+      <div className="flex space-x-10 mx-auto">
+        <button
+          className="p-2 bg-red-600 hover:bg-red-700 text-white border rounded-full px-4"
+          disabled={page == 0}
+          onClick={() => {
+            setPage((currentPage) => currentPage - 1)
+          }}
+        >
+          Prev
+        </button>
+        <button
+          className="p-2 bg-green-500 hover:bg-green-600 text-white border rounded-full px-4"
+          disabled={page == steps.length - 1}
+          onClick={() => {
+            setPage((currentPage) => currentPage + 1)
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
