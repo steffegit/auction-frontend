b3733685565dea7b83f3141db89e1cacda9f1366
CodexDEV <stefanga2020@gmail.com>
2022-06-07 23:09:55 +0300

    Extracted all values from JSON.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../components/Context/UserContext'
 
 function Profile() {
-  return <div>Profile</div>
+  const { users } = useContext(UserContext)
+
+  return (
+    <div>
+      {/* <pre>{JSON?.stringify(users, null, 2)}</pre> */}
+      <div className="flex flex-col sm:flex-row max-w-7xl mx-auto p-5">
+        <div id="#leftside">
+          <img
+            src={users?.picture}
+            alt="avatar"
+            className="w-1/2 aspect-square object-cover rounded-full"
+          />
+        </div>
+        <div id="#rightside">
+          <div>{users?.name}</div>
+          <div>{users?.username}</div>
+          <div>{users?.email}</div>
+          <div>{users?.location}</div>
+          {/* ADD A CHECK LIKE ON TWITTER ON INSTAGRAM IF VERIFIED */}
+          <div>{JSON.stringify(users?.verified)}</div>
+
+          <div>{users?.bids}</div>
+          <div>{users?.sells}</div>
+          <div>{users?.status}</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
