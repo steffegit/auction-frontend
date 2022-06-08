a468566241390f5d42df0ad0f0c297b6eba222f7
CodexDEV <stefanga2020@gmail.com>
2022-06-08 22:17:15 +0300

    Verified.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,15 +5,20 @@ function Profile() {
   const { users } = useContext(UserContext)
 
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-center space-y-6 sm:space-y-0 max-w-7xl mx-auto p-5 sm:space-x-10">
-      <div id="#leftside" className="w-1/2">
+    <div className="flex flex-col sm:flex-row justify-center items-center sm:justify-start sm:items-start space-y-6 sm:space-y-0 max-w-7xl mx-auto p-5 sm:space-x-10">
+      <div id="#leftside" className="relative w-1/3">
         <img
           src={users?.picture}
           alt="avatar"
-          className="aspect-square object-cover rounded-full sm:rounded-none"
+          className="aspect-square object-cover rounded-full sm:rounded-md"
         />
+        {users?.verified ? (
+          <div className="absolute top-0 right-0 font-bold p-2 bg-green-400 rounded-r-md text-xs text-green-900">
+            VERIFIED
+          </div>
+        ) : null}
       </div>
-      <div id="#rightside">
+      <div id="#rightside" className="text-center sm:text-left">
         <div>{users?.name}</div>
         <div>{users?.username}</div>
         <div>{users?.email}</div>
