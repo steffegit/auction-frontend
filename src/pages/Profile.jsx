edb1438e6b35c57f3422bcebb3e96ffaa5972f11
CodexDEV <stefanga2020@gmail.com>
2022-06-08 21:51:50 +0300

    Finally fixed the flex bug on profile.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,28 +5,25 @@ function Profile() {
   const { users } = useContext(UserContext)
 
   return (
-    <div>
-      {/* <pre>{JSON?.stringify(users, null, 2)}</pre> */}
-      <div className="flex flex-col sm:flex-row max-w-7xl mx-auto p-5">
-        <div id="#leftside">
-          <img
-            src={users?.picture}
-            alt="avatar"
-            className="w-1/2 aspect-square object-cover rounded-full"
-          />
-        </div>
-        <div id="#rightside">
-          <div>{users?.name}</div>
-          <div>{users?.username}</div>
-          <div>{users?.email}</div>
-          <div>{users?.location}</div>
-          {/* ADD A CHECK LIKE ON TWITTER ON INSTAGRAM IF VERIFIED */}
-          <div>{JSON.stringify(users?.verified)}</div>
+    <div className="flex flex-col sm:flex-row justify-center items-center space-y-6 sm:space-y-0 max-w-7xl mx-auto p-5 sm:space-x-10">
+      <div id="#leftside" className="w-1/2">
+        <img
+          src={users?.picture}
+          alt="avatar"
+          className="aspect-square object-cover rounded-full sm:rounded-none"
+        />
+      </div>
+      <div id="#rightside">
+        <div>{users?.name}</div>
+        <div>{users?.username}</div>
+        <div>{users?.email}</div>
+        <div>{users?.location}</div>
+        {/* ADD A CHECK LIKE ON TWITTER ON INSTAGRAM IF VERIFIED */}
+        <div>{JSON.stringify(users?.verified)}</div>
 
-          <div>{users?.bids}</div>
-          <div>{users?.sells}</div>
-          <div>{users?.status}</div>
-        </div>
+        <div>{users?.bids}</div>
+        <div>{users?.sells}</div>
+        <div>{users?.status}</div>
       </div>
     </div>
   )
