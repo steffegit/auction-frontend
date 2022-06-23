3b94ef4655a47611c22590d400c69d1faa2f0de3
CodexDEV <stefanga2020@gmail.com>
2022-06-23 17:22:34 +0300

    Added labels to Login and Register Form.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,25 +14,33 @@ function Login() {
     <div className="flex flex-col p-5 py-20 max-w-7xl mx-auto rounded-sm">
       <div className="text-3xl font-bold self-center mb-10">Login</div>
       <form
-        className="flex flex-col space-y-14 max-w-xl mx-auto"
+        className="flex flex-col space-y-14 max-w-xl mx-auto w-full"
         onSubmit={handleSubmit((data) => {
           logProfile(data)
           navigate('/', { replace: true })
         })}
       >
         <div className="space-y-4">
-          <input
-            {...register('email')}
-            type="email"
-            placeholder="mail@mail.com"
-            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
-          />
-          <input
-            {...register('password')}
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
-          />
+          <div className="w-full space-y-1">
+            <label htmlFor="email">Email</label>
+            <input
+              {...register('email')}
+              type="email"
+              name="email"
+              placeholder="johndoe@mail.com"
+              className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            />
+          </div>
+          <div className="w-full space-y-1">
+            <label htmlFor="password">Password</label>
+            <input
+              {...register('password')}
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            />
+          </div>
         </div>
         <button
           type="submit"
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ function Register() {
     <div className="flex flex-col p-5 py-20 max-w-7xl mx-auto rounded-sm">
       <div className="text-3xl font-bold self-center mb-10">Register</div>
       <form
-        className="flex flex-col space-y-14 max-w-xl mx-auto"
+        className="flex flex-col space-y-14 max-w-xl mx-auto w-full"
         onSubmit={handleSubmit((data) => {
           createRegisterUser(data)
           alert('You have registered. Go to login.')
@@ -21,24 +21,36 @@ function Register() {
         })}
       >
         <div className="space-y-4">
-          <input
-            {...register('name')}
-            type="text"
-            placeholder="Name"
-            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
-          />
-          <input
-            {...register('email')}
-            type="email"
-            placeholder="Type your email here..."
-            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
-          />
-          <input
-            {...register('password')}
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
-          />
+          <div className="space-y-1">
+            <label htmlFor="name">Name</label>
+            <input
+              {...register('name')}
+              type="text"
+              placeholder="ex: John Doe"
+              name="name"
+              className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            />
+          </div>
+          <div className="space-y-1">
+            <label htmlFor="email">Email</label>
+            <input
+              {...register('email')}
+              type="email"
+              name="email"
+              placeholder="Type your email here..."
+              className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            />
+          </div>
+          <div className="space-y-1">
+            <label htmlFor="password">Password</label>
+            <input
+              {...register('password')}
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="w-full p-3 rounded-md border border-slate-400 focus:outline-none focus:border-slate-500"
+            />
+          </div>
         </div>
         <button
           type="submit"
