670f6a4667ab75bdd0577788629f799cb7f7ada8
CodexDev <stefanga2020@gmail.com>
2022-07-08 19:09:36 +0300

    Set up modal for post, for testing purposes.

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,34 @@
 import React, { useContext } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { SiteContext } from '../components/Context/Context'
+
+const predefinedImages = [
+  {
+    id: 1,
+    url: 'https://www.reezocar.com/raw/autoscout24.it/RZCATSIT7153BB4BE6E1/MAZDA-MX-5-00.jpg',
+  },
+  {
+    id: 2,
+    url: 'https://www.cnet.com/a/img/resize/4688d2ce6693cc1e4953e63bc3df1a0b32b3dba2/hub/2022/05/05/727e79b6-dd72-40c8-9839-bbc3227dbd19/miata-ogi.jpg?auto=webp&fit=crop&height=675&width=1200',
+  },
+  {
+    id: 3,
+    url: 'https://thumbs.dreamstime.com/b/classic-red-mazda-mx-na-series-i-mazda-miata-front-cluj-napoca-romania-april-two-seat-convertible-roadster-retractable-70841855.jpg',
+  },
+]
 
 function Post() {
   const { brands } = useContext(SiteContext)
+  const [showModal, setShowModal] = useState(false)
+  const [imgUrls, setImgUrls] = useState([])
+
+  const activateModal = () => setShowModal(true)
+  const hideModal = () => setShowModal(false)
+
+  useEffect(() => {
+    console.log(imgUrls)
+  }, [imgUrls])
 
   return (
     <div className="max-w-3xl mx-auto p-3 py-20 flex flex-col items-center justify-start sm:p-12 sm:py-20 rounded-md bg-white">
@@ -81,7 +107,7 @@ function Post() {
               />
             </label>
           </div>
-          <div>
+          {/* <div>
             <label className="w-full flex flex-col mx-auto p-4 items-center cursor-pointer bg-white rounded-md text-black border border-slate-400 focus:outline-none focus:border-slate-500">
               <input type="file" className="hidden" />
               <svg
@@ -98,7 +124,41 @@ function Post() {
               </svg>
               <div>Upload</div>
             </label>
+          </div> */}
+          <div onClick={activateModal}>
+            <label className="w-full flex flex-col mx-auto p-4 items-center cursor-pointer bg-white rounded-md text-black border border-slate-400 focus:outline-none focus:border-slate-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10"
+                viewBox="0 0 20 20"
+                fill="black"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <div>Upload</div>
+            </label>
           </div>
+        </div>
+        {showModal ? (
+          <div className="flex overflow-scroll space-x-4">
+            {predefinedImages.map((item, idx) => (
+              <img
+                src={item.url}
+                className="w-[248px] aspect-video object-cover"
+                onClick={() => {
+                  setImgUrls((oldUrls) => [...oldUrls, item.url])
+                  hideModal()
+                }}
+              />
+            ))}
+          </div>
+        ) : null}
+        <div className="text-xl font-medium">
+          You have uploaded {imgUrls.length} images.
         </div>
         <button
           type="submit"
